651cfe465e89fb7805d070c42faa664a15e6584b
Korbakova Olga <korbolga@ya.ru>
2021-12-13 22:42:01 +0300

    task_1

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,7 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-    let messege = productName;
+    
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -13,7 +13,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
+    let message = productName + ' за ' + productPrice + ' теперь в корзине!';
 
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -23,7 +25,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
 
+    newValue = newValue + 1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -36,9 +40,13 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+    
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum;
+    let newSumText;
 
+    newSum = oldSum + difference;
+    newSumText = newSum + ' ₽'
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
