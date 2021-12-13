5b482b20ea42803413fbf50afbf2a1b0c628bae5
Korbakova Olga <korbolga@ya.ru>
2021-12-13 21:36:06 +0300

    let massage

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,7 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    let messege = productName;
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
